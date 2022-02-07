345002dd8391ee24bb16a23a86c8d4a13439c63f
defycab <love.bellinder@gmail.com>
2022-02-07 09:57:39 +0100

    add namespace and index route

--- spec/requests/recipes/index_spec.rb
+++ spec/requests/recipes/index_spec.rb
@@ -6,12 +6,10 @@ RSpec.describe 'GET /api/recipes', type: :request do
       get '/api/recipes'
     end
 
-    it 'is expected to respond with status 200' do
-      expect(subject.status).to eq 200
-    end
+    it { is_expected.to have_http_status :ok }
 
     it 'is expected to respond with an empty array' do
-      expect(response_json).to eq []
+      expect(response_json['recipes'].first['title']).to eq 'Fried rice with kimchi'
     end
   end
 end
