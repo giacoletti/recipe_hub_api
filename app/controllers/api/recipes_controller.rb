20811d1e880fa9a9b9ad066a480ea1ed6902193e
Thomas Ochman <thomas@dragster.se>
2022-02-09 15:38:12 +0100

    adds sad path for non-existant id refactors functionality to before actions and controller wide rescue method

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,29 +1,34 @@
 class Api::RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :update]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404_error
+
   def index
     recipes = Recipe.by_recently_created.limit(30)
     render json: recipes, each_serializer: Recipe::IndexSerializer
   end
 
   def show
-    recipe = Recipe.find(params['id'])
-    render json: recipe, serializer: Recipe::ShowSerializer
-  rescue ActiveRecord::RecordNotFound => e
-    render_error('Recipe not found', 404)
+    render json: @recipe, serializer: Recipe::ShowSerializer
   end
 
   def update
-    recipe = Recipe.find(params[:id])
-    recipe.update(recipe_params)
+    @recipe.update(recipe_params)
     render json: { message: 'Your recipe was updated.' }
   end
+  
 
   private
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
   def recipe_params
     params[:recipe].permit(:name, :instructions)
   end
 
-  def render_error(message, status)
-    render json: { message: message }, status: status
+  def render_404_error
+    render json: { message: 'Recipe not found' }, status: 404
   end
+
+  
 end
--- spec/requests/recipes/update_spec.rb
+++ spec/requests/recipes/update_spec.rb
@@ -2,13 +2,30 @@ RSpec.describe 'PUT /api/recipes/:id', type: :request do
   let(:recipe) { create(:recipe) }
 
   subject { response }
-  before do
-    put "/api/recipes/#{recipe.id}", params: { recipe: { name: 'My new recipie' } }
+
+  describe 'successfully' do
+    before do
+      put "/api/recipes/#{recipe.id}", params: { recipe: { name: 'My new recipie' } }
+    end
+
+    it { is_expected.to have_http_status 200 }
+
+    it 'is expected to responde with a message' do
+      expect(response_json['message']).to eq 'Your recipe was updated.'
+    end
   end
 
-  it { is_expected.to have_http_status 200 }
+  describe 'unsuccessfully' do
+    describe 'with non-existent id' do
+      before do
+        put '/api/recipes/9999999', params: { recipe: { name: 'My new recipie' } }
+      end
 
-  it 'is expected to responde with a message' do
-    expect(response_json['message']).to eq 'Your recipe was updated.'
+      it { is_expected.to have_http_status 404 }
+
+      it 'is expected to respond with error message' do
+        expect(response_json['message']).to eq 'Recipe not found'
+      end
+    end
   end
 end
