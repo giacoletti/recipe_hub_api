ef5d108feba4abf0957b18ab245fce57c97fc071
Mljungho <mathias.ljungholm@gmail.com>
2022-02-10 18:20:12 +0100

    Adding test for filtering recipes by user

--- spec/requests/api/recipes/index_spec.rb
+++ spec/requests/api/recipes/index_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe 'GET /api/recipes', type: :request do
   subject { response }
   let!(:recipe) { 30.times { create(:recipe) } }
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
   describe 'successfully' do
     before do
       get '/api/recipes'
@@ -18,6 +20,18 @@ RSpec.describe 'GET /api/recipes', type: :request do
 
     it 'is expected to respond the with right instructions' do
       expect(response_json['recipes'].first['instructions']).to eq 'Mix everything'
+    end
+  end
+
+  describe 'filter recipes by user' do
+    before do
+      get '/api/recipes', headers: credentials
+    end
+
+    it { is_expected.to have_http_status :ok }
+
+    it 'is expected to respond with a collection of recipes that belongs to a user' do
+      expect(response_json['recipes'].all? { |recipe| recipe['user'] == user.email }).to eq true
     end
   end
 end
