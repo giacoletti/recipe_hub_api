a82b3110c209ec240d2345f9261f316420e3358e
Mljungho <mathias.ljungholm@gmail.com>
2022-02-13 19:17:27 +0100

    Add owner & email in show serializer

--- app/serializers/recipe/show_serializer.rb
+++ app/serializers/recipe/show_serializer.rb
@@ -1,8 +1,12 @@
 class Recipe::ShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :instructions, :ingredients, :created_at
+  attributes :id, :name, :instructions, :ingredients, :created_at, :owner
   has_many :ingredients, serializer: Ingredient::ShowSerializer
 
   def created_at
     object.created_at.to_formatted_s(:long)
+  end
+
+  def owner
+    object.user.email
   end
 end
