17ab91962b7b681b527dd9668cc1bd98d396b953
giacoletti <giacoletti92@gmail.com>
2022-02-15 21:17:25 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_105855) do
+ActiveRecord::Schema.define(version: 2022_02_15_194951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_02_15_105855) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.integer "forks_count", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
