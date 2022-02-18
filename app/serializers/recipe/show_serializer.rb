5ec6571daf11a273a2aed08ad8e7f22df1a1ac61
defycab <love.bellinder@gmail.com>
2022-02-18 11:25:47 +0100

    add image to show serializer update show serializer spec

--- app/serializers/recipe/show_serializer.rb
+++ app/serializers/recipe/show_serializer.rb
@@ -1,5 +1,5 @@
 class Recipe::ShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :instructions, :ingredients, :created_at, :owner, :comments, :forks_count
+  attributes :id, :name, :instructions, :ingredients, :created_at, :owner, :comments, :forks_count, :image
   has_many :ingredients, serializer: Ingredient::ShowSerializer
   has_many :comments, serializer: Comment::ShowSerializer
 
@@ -9,5 +9,9 @@ class Recipe::ShowSerializer < ActiveModel::Serializer
 
   def owner
     object.user.email
+  end
+
+  def image
+    object.image_serialized
   end
 end
